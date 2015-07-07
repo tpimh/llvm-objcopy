2beefa16cb82198711dd0dca53413af8312d3c3f
Modkit <modkit@Modkits-MacBook-Air.local>
2015-07-06 23:41:29 -0400

    update for latest llvm

--- llvm-objcopy.cpp
+++ llvm-objcopy.cpp
@@ -26,7 +26,6 @@
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/system_error.h"
 
 using namespace llvm;
 using namespace object;
@@ -36,29 +35,12 @@ InputFilename(cl::Positional, cl::desc("<input object file>"), cl::Required);
 static cl::opt<std::string>
 OutputFilename(cl::Positional, cl::desc("<output object file>"), cl::Required);
 
-namespace {
-    enum OutputFormatTy { binary, intel_hex, readmemh };
-    cl::opt<OutputFormatTy>
-        OutputTarget("O",
-                cl::desc("Specify output target"),
-                cl::values(clEnumVal(binary,    "raw binary"),
-                           clEnumVal(intel_hex, "Intel Hex format"),
-                           clEnumVal(readmemh,  "Format read by Verilog's $readmemh system task"),
-                           clEnumValEnd),
-                cl::init(binary));
-    cl::alias OutputTarget2("output-target", cl::desc("Alias for -O"),
-            cl::aliasopt(OutputTarget));
+static StringRef ToolName;
 
-    static StringRef ToolName;
-}
-
-bool llvm::error(error_code ec) {
+bool error(std::error_code ec) {
     if (!ec) return false;
-
-    errs() << ToolName << ": error reading file: " << ec.message() << ".\n";
     return true;
 }
-
 class ObjectCopyBase {
 public:
     ObjectCopyBase(StringRef InputFilename) 
@@ -73,36 +55,29 @@ public:
             return;
         }
 
-        std::string ErrorInfo;
+        std::error_code ErrorCode;
 
-        tool_output_file Out(OutputFilename.data(), ErrorInfo, mBinaryOutput ? sys::fs::F_Binary : sys::fs::F_None);
-        if (!ErrorInfo.empty()) {
-            errs() << ErrorInfo << '\n';
-            return;
-        }
-
-        error_code  ec;
+        tool_output_file Out(OutputFilename.data(), ErrorCode,  sys::fs::F_None);
+        
+        std::error_code  ec;
         bool        FillNextGap = false;
-        uint64_t    LastAddress;
+        uint64_t    LastAddress = 0;
         StringRef   LastSectionName;
 
-        for (section_iterator si = o->begin_sections(), se = o->end_sections(); si != se; si.increment(ec)) {
-            if (error(ec)) return;
-
-            StringRef SectionName;
+        for (section_iterator si = o->section_begin(), se = o->section_end(); si != se; ++si) {
+                       StringRef SectionName;
             StringRef SectionContents;
             uint64_t  SectionAddress;
             bool      BSS;
             bool      Required;
 
+   
             if (error(si->getName(SectionName))) return;
             if (error(si->getContents(SectionContents))) return;
-            if (error(si->getAddress(SectionAddress))) return;
-            if (error(si->isBSS(BSS))) continue;
-            if (error(si->isRequiredForExecution(Required))) continue;
-
-            if (   !Required
-                || BSS
+            SectionAddress = si->getAddress();
+            BSS = si->isBSS();
+            
+            if ( BSS
                 || SectionContents.size() == 0) {
                 continue;
             }
@@ -144,70 +119,7 @@ protected:
     bool                  mFillGaps;
 };
 
-class ObjectCopyIntelHex : public ObjectCopyBase {
-public:
-    ObjectCopyIntelHex(StringRef InputFilename) 
-        : ObjectCopyBase(InputFilename) {}
-    virtual ~ObjectCopyIntelHex() {}
 
-protected:
-    virtual void PrintSection(tool_output_file &Out, const StringRef &SectionName,
-                              const StringRef &SectionContents, uint64_t SectionAddress) const
-    {
-        uint64_t LastBaseAddr = UINT64_MAX;
-
-        Out.os() << "; Contents of section " << SectionName << "(@" << format("%08" PRIx64, SectionAddress) << "):\n";
-
-        // Dump out content as Intel-Hex.
-        uint64_t addr;
-        uint64_t end;
-        for (addr = 0, end = SectionContents.size(); addr < end; addr += 16) {
-            uint64_t      LineAddr = SectionAddress + addr;
-            uint64_t      Base = LineAddr >> 16;
-            uint64_t      Size = (addr + 16 > end) ? end-addr : 16;
-            unsigned char Sum;
-
-            if (LastBaseAddr != Base) {
-                Sum = 6 + (Base & 0xff) + ((Base >> 8) & 0xff);
-                Out.os() << format(":02000004%04" PRIx64 "%02" PRIx8 "\n", Base, (unsigned char)(-Sum));
-                LastBaseAddr = Base;
-            }
-
-            // Dump line header.
-            Sum = Size + (LineAddr & 0xff) + ((LineAddr >> 8) & 0xff);
-            Out.os() << format(":%02" PRIx64 "%04" PRIx64 "00", Size, LineAddr & 0xffff);
-
-            // Dump line of hex.
-            for (uint64_t i = 0; i < Size; ++i) {
-                Sum += SectionContents[addr + i];
-                Out.os() << format("%02" PRIx8, (unsigned char)(SectionContents[addr + i]));
-            }
-            // Dump checksum byte.
-            Out.os() << format("%02" PRIx8 "\n", (unsigned char)(-Sum));
-        }
-    }
-};
-
-class ObjectCopyReadMemH : public ObjectCopyBase {
-public:
-    ObjectCopyReadMemH(StringRef InputFilename) 
-        : ObjectCopyBase(InputFilename) {}
-    virtual ~ObjectCopyReadMemH() {}
-
-protected:
-    virtual void PrintSection(tool_output_file &Out, const StringRef &SectionName,
-                              const StringRef &SectionContents, uint64_t SectionAddress) const
-    {
-        // Dump address
-        Out.os() << "@" << format("%" PRIx64, SectionAddress) << "\n";
-        uint64_t addr;
-        uint64_t end;
-        for (addr = 0, end = SectionContents.size(); addr < end; ++addr) {
-            // Dump hex value.
-            Out.os() << format("%02" PRIx8 "\n", (unsigned char)(SectionContents[addr]));
-        }
-    }
-};
 
 class ObjectCopyBinary : public ObjectCopyBase {
 public:
@@ -248,41 +160,31 @@ int main(int argc, char **argv) {
     cl::ParseCommandLineOptions(argc, argv, "llvm object file copy utility\n");
 
     ToolName = argv[0];
-    OwningPtr<ObjectCopyBase> ObjectCopy;
+    std::unique_ptr<ObjectCopyBase> ObjectCopy;
 
-    switch (OutputTarget) {
-    case OutputFormatTy::binary:
-        ObjectCopy.reset(new ObjectCopyBinary(InputFilename));
-        break;
-    case OutputFormatTy::intel_hex:
-        ObjectCopy.reset(new ObjectCopyIntelHex(InputFilename));
-        break;
-    case OutputFormatTy::readmemh:
-        ObjectCopy.reset(new ObjectCopyReadMemH(InputFilename));
-        break;
-    default:
-        return 1;
-    }
+    ObjectCopy.reset(new ObjectCopyBinary(InputFilename));
+    
 
     // If file isn't stdin, check that it exists.
     if (InputFilename != "-" && !sys::fs::exists(InputFilename)) {
         errs() << ToolName << ": '" << InputFilename << "': " << "No such file\n";
         return 1;
     }
-
-    // Attempt to open  binary.
-    OwningPtr<Binary> binary;
-    if (error_code ec = createBinary(InputFilename, binary)) {
-        errs() << ToolName << ": '" << InputFilename << "': " << ec.message() << ".\n";
+   
+    // Attempt to open the binary.
+    ErrorOr<OwningBinary<Binary>> BinaryOrErr = createBinary(InputFilename);
+    if (std::error_code EC = BinaryOrErr.getError()) {
+        //reportError(File, EC);
+        return 1;
+    }
+    Binary &Binary = *BinaryOrErr.get().getBinary();
+    
+    ObjectFile *Obj = dyn_cast<ObjectFile>(&Binary);
+    if(!Obj){
         return 1;
     }
 
-    ObjectFile *o = dyn_cast<ObjectFile>(binary.get());
-    if (o == NULL) {
-        errs() << ToolName << ": '" << InputFilename << "': " << "Unrecognized file type.\n";
-    }
-
-    ObjectCopy->CopyTo(o, OutputFilename);
+    ObjectCopy->CopyTo(Obj, OutputFilename);
 
     return 0;
 }
